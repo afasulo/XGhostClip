57b5d856cdd073803df63df95841a08c1a3e7ad6
afasulo <afasulo3@gmail.com>
2025-09-24 12:05:51 -0600

    Initial Commit

---
+++ PrimaryPaste/PrimaryPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimaryPasteApp.swift
+//  PrimaryPaste
+//
+//  Created by Adam Fasulo on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct PrimaryPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
